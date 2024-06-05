9962bfb8a36d586ae721e0b09b52eff7b2034ca8
Gabriel Galang <galang69420@gmail.com>
2024-06-05 17:59:11 +0700

    major revision

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function toggleDisplay(element) {
+  if (element.style.display == 'block') {
+       element.style.display = 'none';
+  } else {
+      element.style.display = 'block';
+  };
+};
+
+function toggleDisplayFlex(element) {
+  if (element.style.display == 'flex') {
+       element.style.display = 'none';
+  } else {
+      element.style.display = 'flex';
+  };
+}
